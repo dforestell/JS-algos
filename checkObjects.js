86feb19fee9ecfdb264df8907918234f0113900d
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-09-02 15:49:32 -0500

    change it so that it works regardless of number of keys

--- checkObjects.js
+++ checkObjects.js
@@ -4,10 +4,17 @@
 // included in the returned array.
 
 function whatIsInAName(collection, source) {
-    let arr = [];
-    let keys = Object.keys(source)
-    arr = collection.filter(obj => obj.hasOwnProperty(keys[0]) && obj[keys[0]] === source[keys[0]])
-    return arr;
-  }
+    var keys = Object.keys(source);
+
+    return collection.filter(function (obj) {
+        return keys.every(function (key) {
+            return obj.hasOwnProperty(key) && obj[key] === source[key];
+        });
+    });
+}
+
   
-  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+
+  console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }))
+
